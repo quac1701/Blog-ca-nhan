dd93a9c0cafa25c85daf6665912686ae1ccc8f6b
quac17 <quac1701@gmail.com>
2022-07-18 03:13:17 +0700

    Crete APP + mongoDB connected

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <p>Blog</p>
+  );
+}
+
+export default App;
---
+++ server/controllers/posts.js
@@ -0,0 +1,15 @@
+import { PostModel } from "../models/postModel.js";
+
+export const getPosts = async (req, res) => {
+    try{
+        const posts = await PostModel.find();
+        console.log('posts', posts);
+        res.status(200).json(posts);
+    } catch(err){
+        res.status(500).json({error: err});
+    }
+};
+
+export const createPost = (req, res) =>{
+    res.send("CREATE SUCCESS");
+}
