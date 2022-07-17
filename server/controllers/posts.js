c45c2c4c077525321d4526bcb98d2dd7b040386d
quac17 <quac1701@gmail.com>
2022-07-18 03:30:34 +0700

    Add: getPost, addPost, updatePost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,6 +10,31 @@ export const getPosts = async (req, res) => {
     }
 };
 
-export const createPost = (req, res) =>{
-    res.send("CREATE SUCCESS");
+export const createPost = async (req, res) =>{
+    try{
+        const newPost = req.body;
+
+        const post = new PostModel(newPost);
+        await post.save();
+
+        res.status(200).json(posts); 
+    } catch (err) {
+        res.status(500).json({error: err});
+    }
+}
+
+export const updatePost = async (req, res) =>{
+    try{
+        const updatePost = req.body;
+
+        const post = await PostModel.findOneAndUpdate(
+            { _id: updatePost._id},
+            updatePost,
+            {new: true}
+        );
+
+        res.status(200).json(posts); 
+    } catch (err) {
+        res.status(500).json({error: err});
+    }
 }
